307844beb8a68808ef7feaaa50b0d6234792ceb5
oazevedo <oscar.msazevedo@gmail.com>
2025-02-08 18:50:31 +0000

    Update isMyWebsiteInsecure-2.sh

--- isMyWebsiteInsecure-2.sh
+++ isMyWebsiteInsecure-2.sh
@@ -1,1 +1,65 @@
+#!/bin/bash
+# isMyWebsiteInsecure-2.sh
+# developed by Oscar Azevedo, oscar.azevedo@aeportugal.pt, oscar.msazevedo@gmail.com
+# v1.0
+
+# Function to validate URL format
+validate_url() {
+    if [[ ! $1 =~ ^(http|https):// ]]; then
+        echo "Error: The provided parameter is not a valid URL. It should start with http:// or https://."
+        exit 1
+    fi
+}
+
+# Function to check if required tools are installed
+check_tools() {
+    required_tools=(whois dnsrecon whatweb nc wpscan sqlmap curl nmap sslscan nrich dig)
+    for tool in "${required_tools[@]}"; do
+        if ! command -v $tool &> /dev/null; then
+            echo "$tool is not installed. Please install it before running the script."
+            exit 1
+        fi
+    done
+}
+
+# Main script
+main() {
+    # Display ethical use warning
+    echo -e "\n\e[31mWarning: Ensure you have explicit authorization before running these tests. Unauthorized testing is illegal and unethical.\e[0m\n"
+
+    # Ask user if they want to continue
+    read -p "Do you want to continue running the script? (yes/no): " choice
+    if [[ "$choice" != "yes" ]]; then
+        echo "Exiting script."
+        exit 0
+    fi
+
+    # Check if a URL parameter is provided
+    if [ "$#" -ne 1 ]; then
+        echo -e "\e[32mUsage: $0 <url>\e[0m"
+        exit 1
+    fi
+
+    # Validate the URL format
+    url="$1"
+    validate_url "$url"
+
+    # Check if required tools are installed
+    check_tools
+
+    # Extract host and domain
+    host=$(echo "$url" | awk -F[/:] '{print $4}')
+    domain=$(echo "$host" | awk -F. '{if (NF>2) {print $(NF-1)"."$NF} else {print $0}}')
+    # Get the first IPv4 address from DNS
+    ipv4=$(dig +short "$host" | tail -n1)
+
+    echo
+    echo "Host=$host"
+    echo "Domain=$domain"
+    echo "IPv4=$ipv4"
+    echo "Url=$url"
+    echo
+
+
+    
 
