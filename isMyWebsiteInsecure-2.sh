d9bfd7c74f218975c2c6bb6b35e26c30d1efaaeb
oazevedo <oscar.msazevedo@gmail.com>
2025-02-08 20:57:49 +0000

    Update isMyWebsiteInsecure-2.sh

--- isMyWebsiteInsecure-2.sh
+++ isMyWebsiteInsecure-2.sh
@@ -46,65 +46,29 @@ main() {
     echo "Url=$url"
     echo
 
-    chromium --incognito "$url"
-    sleep 10
+    chromium --incognito "$url" &
+    sleep 2
 
-    chromium --incognito --new-tab "https://pentest-tools.com/network-vulnerability-scanning/tcp-port-scanner-online-nmap/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://www.wpsec.com/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://sitecheck.sucuri.net/results/$host"
-    sleep 10
-
-    chromium --incognito --new-tab "https://powerdmarc.com/dkim-record-lookup/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://domain-checker.valimail.com/dmarc/$domain"
-    sleep 10
-
-    chromium --incognito --new-tab "https://www.ssllabs.com/ssltest/analyze.html?d=$url&hideResults=on&latest"
-    sleep 10
-
-    chromium --incognito --new-tab "https://securityheaders.com/?followRedirects=on&hide=on&q=$url"
-    sleep 10
-
-    chromium --incognito --new-tab "https://report-uri.com/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://developer.mozilla.org/en-US/observatory/analyze?host=$host"
-    sleep 10
-
-    chromium --incognito --new-tab "https://internet.nl/site/$host/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://www.immuniweb.com/websec/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://pentest-tools.com/website-vulnerability-scanning/website-scanner"
-    sleep 10
-
-    chromium --incognito --new-tab "https://www.zaproxy.org/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://www.tenable.com/products/nessus/nessus-essentials"
-    sleep 10
-
-    chromium --incognito --new-tab "https://snyk.io/product/snyk-code/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://www.cookiebot.com/en/compliance-test/?domain=$host"
-    sleep 10
-
-    chromium --incognito --new-tab "https://developers.google.com/speed/pagespeed/insights/?url=$url"
-    sleep 10
-
-    chromium --incognito --new-tab "https://validator.w3.org/nu/?showsource=no&doc=$url/"
-    sleep 10
-
-    chromium --incognito --new-tab "https://jigsaw.w3.org/css-validator/validator?uri=$url&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en"
-    sleep 10
+    chromium --incognito --new-tab "https://pentest-tools.com/network-vulnerability-scanning/tcp-port-scanner-online-nmap/" \
+                         --new-tab "https://www.wpsec.com/" \
+                         --new-tab "https://sitecheck.sucuri.net/results/$host" \
+                         --new-tab "https://powerdmarc.com/dkim-record-lookup/" \
+                         --new-tab "https://domain-checker.valimail.com/dmarc/$domain" \
+                         --new-tab "https://www.ssllabs.com/ssltest/analyze.html?d=$url&hideResults=on&latest" \
+                         --new-tab "https://securityheaders.com/?followRedirects=on&hide=on&q=$url" \
+                         --new-tab "https://report-uri.com/" \
+                         --new-tab "https://developer.mozilla.org/en-US/observatory/analyze?host=$host" \
+                         --new-tab "https://internet.nl/site/$host/" \
+                         --new-tab "https://www.immuniweb.com/websec/" \
+                         --new-tab "https://pentest-tools.com/website-vulnerability-scanning/website-scanner" \
+                         --new-tab "https://www.zaproxy.org/" \
+                         --new-tab "https://www.tenable.com/products/nessus/nessus-essentials" \
+                         --new-tab "https://snyk.io/product/snyk-code/" \
+                         --new-tab "https://www.cookiebot.com/en/compliance-test/?domain=$host" \
+                         --new-tab "https://developers.google.com/speed/pagespeed/insights/?url=$url" \
+                         --new-tab "https://validator.w3.org/nu/?showsource=no&doc=$url/" \
+                         --new-tab "https://jigsaw.w3.org/css-validator/validator?uri=$url&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en"
+                         
 }
 
 # Execute main function
