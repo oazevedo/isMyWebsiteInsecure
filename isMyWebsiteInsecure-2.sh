850a32f500f3ab921d33946751580d6b0190b14f
oazevedo <oscar.msazevedo@gmail.com>
2025-02-08 20:12:00 +0000

    Update isMyWebsiteInsecure-2.sh

--- isMyWebsiteInsecure-2.sh
+++ isMyWebsiteInsecure-2.sh
@@ -46,65 +46,65 @@ main() {
     echo "Url=$url"
     echo
 
-    firefox --new-window --private-window "$url"
-    sleep 2
+    chromium --incognito "$url"
+    sleep 10
 
-    firefox --new-tab --private-window "https://pentest-tools.com/network-vulnerability-scanning/tcp-port-scanner-online-nmap/"
-    sleep 2
+    chromium --incognito --new-tab "https://pentest-tools.com/network-vulnerability-scanning/tcp-port-scanner-online-nmap/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://www.wpsec.com/"
-    sleep 2
+    chromium --incognito --new-tab "https://www.wpsec.com/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://sitecheck.sucuri.net/results/$host"
-    sleep 2
+    chromium --incognito --new-tab "https://sitecheck.sucuri.net/results/$host"
+    sleep 10
 
-    firefox --new-tab --private-window "https://powerdmarc.com/dkim-record-lookup/"
-    sleep 2
+    chromium --incognito --new-tab "https://powerdmarc.com/dkim-record-lookup/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://domain-checker.valimail.com/dmarc/$domain"
-    sleep 2
+    chromium --incognito --new-tab "https://domain-checker.valimail.com/dmarc/$domain"
+    sleep 10
 
-    firefox --new-tab --private-window "https://www.ssllabs.com/ssltest/analyze.html?d=$url&hideResults=on&latest"
-    sleep 2
+    chromium --incognito --new-tab "https://www.ssllabs.com/ssltest/analyze.html?d=$url&hideResults=on&latest"
+    sleep 10
 
-    firefox --new-tab --private-window "https://securityheaders.com/?followRedirects=on&hide=on&q=$url"
-    sleep 2
+    chromium --incognito --new-tab "https://securityheaders.com/?followRedirects=on&hide=on&q=$url"
+    sleep 10
 
-    firefox --new-tab --private-window "https://report-uri.com/"
-    sleep 2
+    chromium --incognito --new-tab "https://report-uri.com/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://developer.mozilla.org/en-US/observatory/analyze?host=$host"
-    sleep 2
+    chromium --incognito --new-tab "https://developer.mozilla.org/en-US/observatory/analyze?host=$host"
+    sleep 10
 
-    firefox --new-tab --private-window "https://internet.nl/site/$host/"
-    sleep 2
+    chromium --incognito --new-tab "https://internet.nl/site/$host/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://www.immuniweb.com/websec/"
-    sleep 2
+    chromium --incognito --new-tab "https://www.immuniweb.com/websec/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://pentest-tools.com/website-vulnerability-scanning/website-scanner"
-    sleep 2
+    chromium --incognito --new-tab "https://pentest-tools.com/website-vulnerability-scanning/website-scanner"
+    sleep 10
 
-    firefox --new-tab --private-window "https://www.zaproxy.org/"
-    sleep 2
+    chromium --incognito --new-tab "https://www.zaproxy.org/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://www.tenable.com/products/nessus/nessus-essentials"
-    sleep 2
+    chromium --incognito --new-tab "https://www.tenable.com/products/nessus/nessus-essentials"
+    sleep 10
 
-    firefox --new-tab --private-window "https://snyk.io/product/snyk-code/"
-    sleep 2
+    chromium --incognito --new-tab "https://snyk.io/product/snyk-code/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://www.cookiebot.com/en/compliance-test/?domain=$host"
-    sleep 2
+    chromium --incognito --new-tab "https://www.cookiebot.com/en/compliance-test/?domain=$host"
+    sleep 10
 
-    firefox --new-tab --private-window "https://developers.google.com/speed/pagespeed/insights/?url=$url"
-    sleep 2
+    chromium --incognito --new-tab "https://developers.google.com/speed/pagespeed/insights/?url=$url"
+    sleep 10
 
-    firefox --new-tab --private-window "https://validator.w3.org/nu/?showsource=no&doc=$url/"
-    sleep 2
+    chromium --incognito --new-tab "https://validator.w3.org/nu/?showsource=no&doc=$url/"
+    sleep 10
 
-    firefox --new-tab --private-window "https://jigsaw.w3.org/css-validator/validator?uri=$url&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en"
-    sleep 2
+    chromium --incognito --new-tab "https://jigsaw.w3.org/css-validator/validator?uri=$url&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en"
+    sleep 10
 }
 
 # Execute main function
