44adf50e8be16a46167e991aed4010b3da05eb99
oazevedo <oscar.msazevedo@gmail.com>
2025-02-09 17:54:56 +0000

    Update isMyWebsiteInsecure-1.sh

--- isMyWebsiteInsecure-1.sh
+++ isMyWebsiteInsecure-1.sh
@@ -2,6 +2,7 @@
 # isMyWebsiteInsecure-1.sh
 # developed by Oscar Azevedo, oscar.azevedo@aeportugal.pt, oscar.msazevedo@gmail.com
 # v1.0
+# check the security of a given website with public command line tools
 
 # Function to validate URL format
 validate_url() {
