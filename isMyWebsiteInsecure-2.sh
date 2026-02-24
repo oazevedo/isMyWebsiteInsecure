f247a1e40abeff10f97ca5113b01c92925ef9032
oazevedo <oscar.msazevedo@gmail.com>
2026-02-24 13:03:05 +0000

    Fix Nmap command options and update Dalfox comment

--- isMyWebsiteInsecure-2.sh
+++ isMyWebsiteInsecure-2.sh
@@ -140,16 +140,17 @@ main() {
     # Nmap Open Ports and Service detection
     # -f fragments packets, --mtu 16 evades DPI, --data-length adds random padding,
     # -T2 slows timing to avoid rate-based detection, --randomize-hosts randomizes order
+	# https://nmap.org/book/man-performance.html
     echo -e "\e[38;5;208m[+] Nmap Open Ports and Service detection...\e[0m"
     echo -e "\e[32m sudo nmap -sS -sV -f --mtu 16 --data-length 25 -T2 --randomize-hosts \"$host\" \e[0m"
-    sudo nmap -sS -sV -f --mtu 16 --data-length 25 -T2 --randomize-hosts "$host"
+    sudo nmap -sS -sV -f --mtu 16 --data-length 25 --randomize-hosts "$host"
     echo -e "\n\n"
 
 
     # Nmap vulnerabilities scan
     echo -e "\e[38;5;208m[+] Nmap vulnerabilities scan...\e[0m"
     echo -e "\e[32m sudo nmap -sS --script vuln -f --mtu 16 --data-length 25 -T2 --randomize-hosts \"$host\" \e[0m"
-    sudo nmap -sS --script vuln -f --mtu 16 --data-length 25 -T2 --randomize-hosts "$host"
+    sudo nmap -sS --script vuln -f --mtu 16 --data-length 25 --randomize-hosts "$host"
     echo -e "\n\n"
 
 
@@ -203,7 +204,7 @@ main() {
 
     vpn_rotate_ip
     # Dalfox XSS Scanner
-    # --waf-evasion already applied
+    # --waf-evasion enable WAF evasion by adjusting speed when detecting WAF (worker=1, delay=3s)  
     echo -e "\e[38;5;208m[+] Dalfox xss scan...\e[0m"
     echo -e "\e[32m dalfox --waf-evasion url \"$url\" \e[0m"
     dalfox --waf-evasion url "$url"
