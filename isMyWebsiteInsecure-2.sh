cc607ab11d230bef8a383cf5f4f6f665f397b4f8
oazevedo <oscar.msazevedo@gmail.com>
2026-03-14 16:52:58 +0000

    Update isMyWebsiteInsecure-2.sh

--- isMyWebsiteInsecure-2.sh
+++ isMyWebsiteInsecure-2.sh
@@ -18,7 +18,6 @@
 #  
 
 
-
 # ──── Evasion User-Agent ──────────────────────────────────────────────────────
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
